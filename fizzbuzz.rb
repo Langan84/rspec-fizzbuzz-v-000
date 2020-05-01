995c345187ac59a336a9c8da7b74795b89446c77
langan.kg Langan <langan.code@gmail.com>
2020-05-01 14:57:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,13 +4,13 @@
 def fizzbuzz(int)
   
   if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
-  "Fizz" # Go Fizz
+  "FizzBuzz" # Go Fizz
 
   elsif int % 5 == 0 # if the number int is divisible by 5
   "Buzz" # Go Buzz
   
   elsif int % 3 == 0 # if the number int is divisible by 3 + 5
-  "FizzBuzz" # Go FizzBuzz
+  "Fizz" # Go FizzBuzz
   end
   
 end
